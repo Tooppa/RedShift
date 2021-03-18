218f05fb9b4f8a1a33d8a6cd00e57a50a51ca6f0
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-03-18 13:45:25 +0200

    Simple movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKeyDown("D"))
+        {
+            _rigidbody2D.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown("A"))
+        {
+            _rigidbody2D.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
+        }
+    }
+}
