033ee3784fb56535ba0503dd41fd010603e46d59
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-03-18 16:07:14 +0200

    Grounding sphere

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,34 +2,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private int speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private LayerMask whatIsGround;
+    private Transform _groundCheck;
     private Rigidbody2D _rigidbody2D;
+    private bool _isGrounded = false;
+    private const float GroundedRadius = .3f;
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _groundCheck = transform.GetChild(0);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        CheckIsGrounded();
         Movement();
     }
-    
+    private void CheckIsGrounded()
+    {
+        var wasGrounded = _isGrounded;
+        _isGrounded = false;
+
+        var colliders = Physics2D.OverlapCircleAll(_groundCheck.position, GroundedRadius, whatIsGround);
+        foreach (var t in colliders)
+        {
+            if (t.gameObject != gameObject)
+            {
+                if (!wasGrounded)
+                    _isGrounded = true;
+            }
+        }
+    }
+
 
     private void Movement()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            _rigidbody2D.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(Vector2.right);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
-            _rigidbody2D.AddForce(Vector2.left * speed, ForceMode2D.Impulse);
+            _rigidbody2D.AddForce(Vector2.left);
         }
     }
 }
