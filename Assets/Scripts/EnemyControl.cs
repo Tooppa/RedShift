55b807dc76c59cc5e4e4a029e4b389dcb57e9a5e
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-03-18 17:17:18 +0200

    EnemyControl start

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+    private Vector2 playerDetectionArea;
+    private Rigidbody2D rb2D;
+
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IdleWalk();
+    }
+
+    void IdleWalk()
+    {
+        rb2D.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
     }
 }
