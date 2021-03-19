c8294e7dcd94dd14fe1759e7305d49ffd1b497cb
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-03-19 16:08:35 +0200

    Pathfinding

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -7,11 +7,16 @@ public class EnemyControl : MonoBehaviour
     private Vector2 playerDetectionArea;
     private Rigidbody2D rb2D;
 
+    private Transform origin;
+    private float rightRange;
+    private float leftRange;
+
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        origin.parent = null;
     }
 
     // Update is called once per frame
