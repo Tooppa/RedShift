bfe2e238f59e9d2cbc3bc6b71e2ce31a4d1ab577
toutoukkanen <tuukka.007@hotmail.com>
2021-03-22 14:17:46 +0200

    Added the option to toggle the flashlight on and off.

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -1,10 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
+[RequireComponent(typeof(Light2D))]
 public class Flashlight : MonoBehaviour
 {
+    private Light2D _light2D;
+    
+    private void Start() => _light2D = GetComponent<Light2D>();
+
     // Update is called once per frame
     private void Update()
     {
+        // Enable and disable the flashlight
+        if (Input.GetKeyDown(KeyCode.F))
+            _light2D.enabled = !_light2D.enabled;
+        
+        // Rotate the flashlight around the user's mouse
+        
         var transformCache = transform;
         
         var flashlightToMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transformCache.position;
@@ -12,7 +25,7 @@ public class Flashlight : MonoBehaviour
         
         var angleFromFlashLightToMouse = Vector3.SignedAngle(transformCache.up, flashlightToMouse, Vector3.forward);
         
-        // Rotate the flashlight by the angle
+        // Rotate the flashlight by the specific angle
         transform.Rotate(Vector3.forward, angleFromFlashLightToMouse);
         
     }
