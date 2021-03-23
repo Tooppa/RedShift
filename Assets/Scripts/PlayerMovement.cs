c4b6f9d4ea115b4fa3d11040b4ada1f174f6b3cd
jereor <jere.orava@hotmail.com>
2021-03-23 14:16:06 +0200

    Implemented jump animation to PlayerFeatures scene. Removed Pixel Space Platformer package.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,9 +15,15 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
     
     private readonly Vector2 _groundCheckOffset = new Vector2(0,-0.5f);
-    private bool _isGrounded = false;
+    public bool _wasGrounded = false;
+    public bool _isGrounded = false;
     private Animator _animator;
     private const float GroundedRadius = 0.1f;
+
+    public bool walking;
+    public bool jumping;
+    public float jumpTimeCounter;
+    public float jumpTime;
 
     private void Awake()
     {
@@ -43,17 +49,39 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(inputDirection, 1, 1);
             _animator.SetBool("Walking", true);
+            walking = true;
         }
         else
         {
             _animator.SetBool("Walking", false);
+            walking = false;
         }
 
         if (Mathf.Abs(_rigidbody2D.velocity.x) < maxSpeed)
             _rigidbody2D.AddForce(Vector2.right * (inputDirection * speed * Time.deltaTime), ForceMode2D.Impulse);
 
         if (_isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("TakeOff");
+            jumping = true;
+            jumpTimeCounter = jumpTime;
             _rigidbody2D.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
-        
+        }
+
+        if (_isGrounded)
+            _animator.SetBool("Jumping", false);
+        else
+            _animator.SetBool("Jumping", true);
+
+        if (Input.GetKey(KeyCode.Space) && jumping == true)
+        {
+            if (jumpTimeCounter > 0)
+                jumpTimeCounter += Time.deltaTime;
+            else
+                jumping = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+            jumping = false;
     }
 }
