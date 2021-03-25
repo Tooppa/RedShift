81b2baa4b84acbdbb5a36476210ac65b36b12de8
jereor <jere.orava@hotmail.com>
2021-03-25 18:17:38 +0200

    Flashlight with 4 directions implemented. Cleaned up unnecessary assets.

--- Assets/Scripts/Player/PlayerGun.cs
+++ Assets/Scripts/Player/PlayerGun.cs
@@ -4,17 +4,11 @@ namespace Player
 {
     public class PlayerGun : MonoBehaviour
     {
-        //public Transform firePoint;
-        //public GameObject bullet;
-        //private GameObject _flashlight;
-        //private Vector2 _shootDirection;
-
         private ParticleSystem gun;
 
         private void Start()
         {
             gun = GetComponentInChildren<ParticleSystem>();
-            //_shootDirection = Vector2.right;
         }
 
         private void Update()
@@ -23,18 +17,6 @@ namespace Player
                 gun.Play();
             else if (Input.GetKeyUp(KeyCode.R))
                 gun.Stop();
-
-            //if (Input.GetKeyDown(KeyCode.G))
-            //{
-            //    //var x = _shootDirection.x;
-            //    //var y = _shootDirection.y;
-            //    //_shootDirection = new Vector2(y, -x);
-            //}
         }
-
-        //void Shoot()
-        //{
-        //    Instantiate(bullet, firePoint.position, firePoint.rotation);
-        //}
     }
 }
