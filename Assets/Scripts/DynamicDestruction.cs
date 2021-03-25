9e492220f1c206a2a9f44f3be2948de306aca0ab
toutoukkanen <tuukka.007@hotmail.com>
2021-03-25 21:20:47 +0200

    Modified Dynamic Destruction to work with any number of pieces.

--- Assets/Scripts/DynamicDestruction.cs
+++ Assets/Scripts/DynamicDestruction.cs
@@ -8,16 +8,25 @@ using UnityEngine;
 public class DynamicDestruction : MonoBehaviour
 {
     public GameObject brokenObjectPrefab;
-    
-    private List<GameObject> parts;
+
+    private readonly List<Transform> _pieces = new List<Transform>(); // All children that have a PolygonCollider
     private Sprite _sprite;  // Used to give the same sprite to the broken pieces
-    
+
     private void Start()
     {
         if (brokenObjectPrefab == null) // Null references, null references everywhere
         {
             Debug.LogWarning("Broken Object prefab not found for " + gameObject.name);
             Destroy(this);
+        }
+
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            Transform child = transform.GetChild(i);
+            
+            // Only add the child if it has a polygon collider
+            if (child.TryGetComponent(out PolygonCollider2D collider)) 
+                _pieces.Add(child);
         }
         
         _sprite = GetComponent<SpriteRenderer>().sprite;
@@ -27,23 +36,26 @@ public class DynamicDestruction : MonoBehaviour
     
     private void BreakApart()
     {
-        // Some debug code to test breaking
-        var firstChild = transform.GetChild(0);
-        var pieceColliderPoints = firstChild.GetComponent<PolygonCollider2D>().points;
+        foreach (var piece in _pieces)
+        {
+            var pieceColliderPoints = piece.GetComponent<PolygonCollider2D>().points;
 
-        // Instantiate new piece without parenting. The new piece has exactly the same location as the original object
-        var newPiece = Instantiate(brokenObjectPrefab, transform.position, transform.rotation);
+            // Instantiate new piece without parenting. The new piece has exactly the same location as the original object
+            var newPiece = Instantiate(brokenObjectPrefab, transform.position, transform.rotation);
         
-        // Set the just instantiated collider's sprite to match the original. This will be automatically masked by it's child
-        newPiece.GetComponent<SpriteRenderer>().sprite = _sprite;
+            // Set the just instantiated collider's sprite to match the original. This will be automatically masked by it's child
+            newPiece.GetComponent<SpriteRenderer>().sprite = _sprite;
         
-        // The new piece also has a child object that has the mask and the collider.
-        // Set it's position and rotation to match the pre-defined piece
-        var newPiecesChild = newPiece.transform.GetChild(0);
-        newPiecesChild.position = firstChild.position;
-        newPiecesChild.rotation = firstChild.rotation;
+            // The new piece also has a child object that has the mask and the collider.
+            // Set it's position and rotation to match the pre-defined piece
+            var newPiecesChild = newPiece.transform.GetChild(0);
+            newPiecesChild.position = piece.position;
+            newPiecesChild.rotation = piece.rotation;
         
-        // Collider points match the one found in the pre-defined piece
-        newPiecesChild.GetComponent<PolygonCollider2D>().points = pieceColliderPoints; 
+            // Collider points match the one found in the pre-defined piece
+            newPiecesChild.GetComponent<PolygonCollider2D>().points = pieceColliderPoints; 
+        }
+        
+
     }
 }
