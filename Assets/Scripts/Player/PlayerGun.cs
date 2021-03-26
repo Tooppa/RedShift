f567ea8fb1760c38680900b3779975a27e6d532b
jereor <jere.orava@hotmail.com>
2021-03-26 17:20:28 +0200

    Added cooldown to shooting.

--- Assets/Scripts/Player/PlayerGun.cs
+++ Assets/Scripts/Player/PlayerGun.cs
@@ -6,20 +6,27 @@ namespace Player
     {
         private ParticleSystem _gun;
 
+        public float cooldown;
+        public float counter;
+
         private void Start()
         {
             _gun = GetComponentInChildren<ParticleSystem>();
+            counter = cooldown;
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.R) && _gun.gameObject.activeSelf)
+            if (Input.GetKeyDown(KeyCode.R) && _gun.gameObject.activeSelf && counter > cooldown)
             {
                 CameraEffects.Instance.ShakeCamera(1.5f, .1f);
                 _gun.Play();
+                counter = 0;
             }
-            else if (Input.GetKeyUp(KeyCode.R))
+            else
                 _gun.Stop();
+
+            counter += Time.deltaTime;
         }
     }
 }
