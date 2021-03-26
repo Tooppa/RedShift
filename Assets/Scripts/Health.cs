84cb073e459b06ee3b9dbcc2b3a7b1ee97e381f7
toutoukkanen <tuukka.007@hotmail.com>
2021-03-26 16:01:26 +0200

    A general lightweight Health script that can be used to link them to other scripts' member functions.
    Added Health-script to the box. Breakable::BreakApart() is called upon death.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public bool destroyWhenDead;
+    
+    public UnityEvent onDie;
+    
+    public float CurrentHealth { get; private set; }
+    public bool Dead { get; private set; }
+    
+    // Start is called before the first frame update
+    private void Start() => CurrentHealth = maxHealth;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+            TakeDamage(50);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+
+        if (CurrentHealth <= 0)
+        {
+            onDie.Invoke();
+            Dead = true;
+            
+            if(destroyWhenDead)
+                Destroy(gameObject);
+        }
+    }
+}
