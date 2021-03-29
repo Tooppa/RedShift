7af806d21cc9ef680e86c4a0732d864d960a9af4
toutoukkanen <tuukka.007@hotmail.com>
2021-03-29 16:31:21 +0300

    Groundwork before pulling upstream changes.

--- Assets/Scripts/Environment/AutomaticShadowCaster2D.cs
+++ Assets/Scripts/Environment/AutomaticShadowCaster2D.cs
@@ -1,4 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+/// Creates a Shadow Caster 2D to the gameobject if a supported Collider 2D is found.
+/// Supported colliders are PolygonCollider2D and CompositeCollider2D.
+/// </summary>
 public class AutomaticShadowCaster2D : MonoBehaviour
-{}
+{
+    private void Start()
+    {
+        // Find a supported collider type
+        if (TryGetComponent(out PolygonCollider2D polygonCollider2D))
+        {}
+    }
+
+}
