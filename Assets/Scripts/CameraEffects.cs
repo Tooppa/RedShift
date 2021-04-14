9262edaa9ce0d260803b36f4cf189051ced6a9bd
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-04-14 15:40:01 +0300

    Better particles

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -47,11 +47,15 @@ public class CameraEffects : MonoBehaviour
 
     private IEnumerator RocketScene()
     {
-        Instantiate(particle,transform);
+        Instantiate(particle,_cam.Follow);
         ShakeCamera(3, 1);
-        yield return new WaitForSeconds(1.5f);
-        Instantiate(particle,transform);
+        Instantiate(particle,_cam.Follow);
+        yield return new WaitForSeconds(.5f);
+        Instantiate(particle,_cam.Follow);
+        yield return new WaitForSeconds(1);
+        Instantiate(particle,_cam.Follow);
         ShakeCamera(5, .1f);
+        Instantiate(particle,_cam.Follow);
     }
 
     public void ChangeOffset(float timer, float x)
