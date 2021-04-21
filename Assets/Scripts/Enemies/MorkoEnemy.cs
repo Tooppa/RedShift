fa7a344013b9b16fa0242023eb6f8b5c492f03e0
toutoukkanen <tuukka.007@hotmail.com>
2021-04-21 17:25:30 +0300

    A* was useless for platforming AI. Rewrote Mörkö to not use that.

--- Assets/Scripts/Enemies/MorkoEnemy.cs
+++ Assets/Scripts/Enemies/MorkoEnemy.cs
@@ -1,7 +1,5 @@
-using Pathfinding;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Seeker), typeof(Animator))]
 public class MorkoEnemy : MonoBehaviour
 {
     public EnemyScriptable data;
@@ -9,21 +7,10 @@ public class MorkoEnemy : MonoBehaviour
     private Transform _player;
 
     private Rigidbody2D _playerRigidbody2D;
-
-    [SerializeField] private float nextWaypointDistance = 2.5f;
     
-    private bool _isGrounded = false;
-    private readonly Vector2 _groundCheckOffset = new Vector2(0, -2.335f);
-    private const float GroundedRadius = 0.45f;
-    [SerializeField] private LayerMask whatIsGround;
-
     private readonly Vector2 _legOffset = new Vector2(0,-2.3f);
-    [SerializeField] private float distanceFromRaycast;
+    [SerializeField] private float distanceToTriggerJump;
 
-    private Path _path;
-    private int _currentWaypoint = 0;
-
-    private Seeker _seeker;
     private Rigidbody2D _rigidbody2D;
 
     private Animator _animator;
@@ -35,12 +22,11 @@ public class MorkoEnemy : MonoBehaviour
         {
             Debug.LogWarning($"No scriptable object for {gameObject.name}");
             this.enabled = false;
-            return;
         }
     }
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _player = GameObject.FindWithTag("Player").transform;
 
@@ -52,57 +38,30 @@ public class MorkoEnemy : MonoBehaviour
         if (_playerRigidbody2D == null)
             this.enabled = false;
         
-        _seeker = GetComponent<Seeker>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-
-        InvokeRepeating(nameof(UpdatePath), 0f, 0.2f);
     }
     
-    void UpdatePath()
-    {
-        if (_seeker.IsDone())
-            _seeker.StartPath(_rigidbody2D.position, _player.transform.position, OnPathComplete);
-    }
-    
-    void OnPathComplete(Path p)
-    {
-        if (p.error) return;
-        
-        _path = p;
-        _currentWaypoint = 0;
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (_path == null)
-            return;
-     
         var positionCache = transform.position;
         
         var distanceToPlayer = Vector2.Distance(positionCache, _player.position); // Always positive
         
         if(distanceToPlayer > data.enemyRange)
             return;
+        
+        if(distanceToPlayer < data.knockbackRadius)
+            Attack();
 
         _animator.SetTrigger(Walk);
         
-        if (_currentWaypoint < _path.vectorPath.Count - 1)
-        {
-            var distanceToWaypoint = Vector2.Distance(_rigidbody2D.position, _path.vectorPath[_currentWaypoint]);
+        // Move towards the player
 
-            if (distanceToWaypoint < nextWaypointDistance)
-            {
-                _currentWaypoint++;
-            }
-        }
+        Vector2 enemyToPlayer = ((Vector2)_player.position - (Vector2) positionCache).normalized;
         
-        // Correct waypoint selected, move towards the waypoint
-
-        Vector2 enemyToWaypoint = ((Vector2)_path.vectorPath[_currentWaypoint] - (Vector2) positionCache).normalized;
-        
-        Vector2 force = enemyToWaypoint * data.speed;
+        Vector2 force = enemyToPlayer * data.speed;
         
         _rigidbody2D.AddForce(force);
         
@@ -115,49 +74,25 @@ public class MorkoEnemy : MonoBehaviour
         var legPosition = (Vector2) positionCache + _legOffset;
 
         // Raycast from the legs by the specified length. Only collide with Ground
-        var hit = Physics2D.Raycast(legPosition, Vector3.right * transformCache.localScale.x, distanceFromRaycast, LayerMask.GetMask("Ground"));
+        var hit = Physics2D.Raycast(legPosition, Vector3.right * transformCache.localScale.x, distanceToTriggerJump, LayerMask.GetMask("Ground"));
 
         if (hit.collider != null)
         {
-            Debug.DrawRay(legPosition, Vector3.right * (distanceFromRaycast * transformCache.localScale.x), Color.red);
+            Debug.DrawRay(legPosition, Vector3.right * (distanceToTriggerJump * transformCache.localScale.x), Color.red);
             _rigidbody2D.AddForce(Vector2.up * 18, ForceMode2D.Impulse);
         }
         else
         {
-            Debug.DrawRay(legPosition, Vector3.right * (distanceFromRaycast * transformCache.localScale.x), Color.gray);
+            Debug.DrawRay(legPosition, Vector3.right * (distanceToTriggerJump * transformCache.localScale.x), Color.gray);
         }
 
     }
+    
+    private void Attack() => PushBack();
 
-    private void Update()
+    private void PushBack()
     {
-        CheckIsGrounded();
-        PlayerHit();
-    }
-
-    private void CheckIsGrounded()
-    {
-        _isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + _groundCheckOffset, GroundedRadius, whatIsGround);
-    }
-
-    private void PlayerHit()
-    {
-        var playerPosition = _player.position;
-        var enemyPosition = transform.position;
-        
-        float distanceX = playerPosition.x - enemyPosition.x;
-        float distanceY = playerPosition.y - enemyPosition.y;
-        
-        if (distanceX <= data.knockbackRadius && distanceX > -data.knockbackRadius && distanceY <= data.knockbackRadius && distanceY > -data.knockbackRadius)
-        {
-            Debug.Log("Hit!");
-            PlayerPushback();
-        }
-    }
-
-    private void PlayerPushback()
-    {
-        Vector2 knockbackDirection = (_player.position - transform.position).normalized;
-        _playerRigidbody2D.AddForce(knockbackDirection * data.knockbackForce);
+        // Direction always from the enemy to the player
+        _playerRigidbody2D.AddForce((_player.position - transform.position) * data.knockbackForce);
     }
 }
