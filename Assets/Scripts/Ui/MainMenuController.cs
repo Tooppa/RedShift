d2149e0dfed9e5d176367aa551f8821917e86ef8
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-04-23 14:49:34 +0300

    Fade to black Animation from menu to game

--- Assets/Scripts/Ui/MainMenuController.cs
+++ Assets/Scripts/Ui/MainMenuController.cs
@@ -1,14 +1,40 @@
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Ui
 {
     public class MainMenuController : MonoBehaviour
     {
         public RectTransform options, credits;
+        public CanvasGroup fader;
         private void Start()
         {
             gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        }
+
+        public void LoadGame()
+        {
+            StartCoroutine(LoadYourAsyncScene());
+        }
+
+        private IEnumerator LoadYourAsyncScene()
+        {
+            var sceneActivation = false;
+            fader
+                .DOFade(1, 1)
+                .OnComplete(() =>
+                {
+                    sceneActivation = true;
+                });
+            var asyncLoad = SceneManager.LoadSceneAsync(1);
+            asyncLoad.allowSceneActivation = sceneActivation;
+            while (!asyncLoad.isDone)
+            {
+                asyncLoad.allowSceneActivation = sceneActivation;
+                yield return new WaitForEndOfFrame();
+            }
         }
 
         public void ExitGame()
