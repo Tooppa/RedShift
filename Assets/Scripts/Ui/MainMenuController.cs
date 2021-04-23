d6f2981e84bec5c6c8b33718e695120f66a33d06
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-04-23 13:23:44 +0300

    animation in main menu

--- Assets/Scripts/Ui/MainMenuController.cs
+++ Assets/Scripts/Ui/MainMenuController.cs
@@ -1,9 +1,11 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Ui
 {
     public class MainMenuController : MonoBehaviour
     {
+        public RectTransform options, credits;
         private void Start()
         {
             gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
@@ -12,6 +14,30 @@ namespace Ui
         public void ExitGame()
         {
             
+        }
+        public void OpenOptions()
+        {
+            options
+                .DOAnchorPos(Vector2.zero, .3f)
+                .SetUpdate(true);
+        }
+        public void OpenCredits()
+        {
+            credits
+                .DOAnchorPos(Vector2.zero, .3f)
+                .SetUpdate(true);
+        }
+        public void CloseOptions()
+        {
+            options
+                .DOAnchorPos(new Vector2(0, -500), .3f)
+                .SetUpdate(true);
+        }
+        public void CloseCredits()
+        {
+            credits
+                .DOAnchorPos(new Vector2(0, -500), .3f)
+                .SetUpdate(true);
         }
     }
 }
