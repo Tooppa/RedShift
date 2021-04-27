b974798a74c8f7fb174a9a5ae1657d47d63cd705
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-04-27 18:33:59 +0300

    More SFX, powerful gunshot charged sound added

--- Assets/Scripts/Player/PlayerGun.cs
+++ Assets/Scripts/Player/PlayerGun.cs
@@ -68,6 +68,7 @@ namespace Player
             CameraEffects.Instance.ShakeCamera(1.5f, .1f);
             _powerShotEffect.Play();
             _audioController.GetComponent<SFX>().playerPowerfulCharge.Stop();
+            _audioController.GetComponent<SFX>().playerPowerfulShotChargedUp.Stop();
             _audioController.GetComponent<SFX>().PlayPowerfulShot();
         }
 
@@ -90,7 +91,8 @@ namespace Player
                 {
                     _chargeEffect.Stop();
                     _chargeReadyEffect.Play();
-                    _audioController.GetComponent<SFX>().PlayPowerfulShotChargedUp();
+                    if(!_audioController.GetComponent<SFX>().playerPowerfulShotChargedUp.isPlaying)
+                        _audioController.GetComponent<SFX>().PlayPowerfulShotChargedUp();
                 }
                 if (HasPowerfulGun && !chargeSFXPlaying)
                 {
