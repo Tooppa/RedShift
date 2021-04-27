b0c7a0e11392c73f0d0d0b3ca6e28a99dfdda77b
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-04-27 16:55:50 +0300

    New SFX, new triggers and audio sources

--- Assets/Scripts/AudioEffects/SFXIntervalandPitchRandomizer.cs
+++ Assets/Scripts/AudioEffects/SFXIntervalandPitchRandomizer.cs
@@ -1,37 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SFXIntervalandPitchRandomizer : MonoBehaviour
 {
-    public AudioSource _audio;
+    public AudioSource[] _audio;
 
-    private SFXPlayCounter counter;
+    private float counter;
 
     public int possibleStartTime;
     public int possibleEndTime;
     public float pitchLowerBorder;
     public float pitchHigherBorder;
 
-    private int sum;
-
     private int rand;
+    private int randomSound;
 
     private void Start()
     {
-        counter = GameObject.Find("SFXPlayCounter").GetComponent<SFXPlayCounter>();
         rand = Random.Range(possibleStartTime, possibleEndTime);
-        sum = rand;
+        randomSound = Random.Range(0, _audio.Length);
     }
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (counter.counter >= sum)
+        counter += Time.deltaTime;
+        if (counter >= rand)
         {
-            _audio.pitch = (float)Random.Range(pitchLowerBorder, pitchHigherBorder);
-            _audio.Play();
+            _audio[randomSound].pitch = (float)Random.Range(pitchLowerBorder, pitchHigherBorder);
+            _audio[randomSound].Play();
             rand = Random.Range(possibleStartTime, possibleEndTime);
-            sum += rand;
+            randomSound = Random.Range(0, _audio.Length);
+            counter = 0;
         }
     }
 }
