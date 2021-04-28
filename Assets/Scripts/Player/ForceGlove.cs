5fa129d39746990bbca698054ecffd5bd0a9af78
Tomas Valkendorff <Tomvalken1@gmail.com>
2021-04-28 14:52:20 +0300

    Animations for spaceship

--- Assets/Scripts/Player/ForceGlove.cs
+++ Assets/Scripts/Player/ForceGlove.cs
@@ -19,12 +19,13 @@ public class ForceGlove : MonoBehaviour
 
     public void Push()
     {
+        if(!HasGlove) return;
         var foundColliders2D = Physics2D.OverlapCircleAll((Vector2) transform.position, pushRadius, whatToPush);
         foreach (var var in foundColliders2D)
         {
             var forceApplier = var.gameObject.AddComponent<ForceApplier>();
             forceApplier.affectedRigidBody2D = var.GetComponent<Rigidbody2D>();
-            var forceVector = (var.transform.position - transform.position);
+            var forceVector = var.transform.position - transform.position;
             forceVector.Normalize();
             forceVector *= forceMultiplier;
             forceApplier.affectVector = forceVector;
