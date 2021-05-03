d4bde61d3507270c3e0126c884e3b5a546daa887
toutoukkanen <tuukka.007@hotmail.com>
2021-05-03 13:28:30 +0300

    Added a new wrapper method to SaveLevelStatusBehaviour that can be used in gameobjects.

--- Assets/Scripts/Player/SaveLevelStatusBehaviour.cs
+++ Assets/Scripts/Player/SaveLevelStatusBehaviour.cs
@@ -6,9 +6,14 @@ namespace Player
     public class SaveLevelStatusBehaviour : MonoBehaviour
     {
         /// <summary>
-        /// This is just forward the call to <see cref="SaveAndLoad.SaveStatus"/> because in the editor it has to be an instance.
+        /// This just forwards the call to <see cref="SaveAndLoad.SaveStatus"/> because in the editor it has to be an instance.
         /// </summary>
         /// <param name="place">Used to execute additional instructions</param>
         public void SaveStatus(string place) => SaveAndLoad.SaveStatus(place);
+
+        /// <summary>
+        /// This just forwards the call to <see cref="SaveAndLoad.StartLoadingSave"/> because in the editor it has to be an instance.
+        /// </summary>
+        public void StartLoadingSave() => SaveAndLoad.StartLoadingSave();
     }
 }
